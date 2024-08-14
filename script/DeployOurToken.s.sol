3652951a209cd94d274445d6129132d4db5dfe60
Muhammad Abdullah <abdullah.se4th@gmail.com>
2024-08-14 17:28:13 +0500

    set the INITIAL_SUPPLY = 100 ethers

--- script/DeployOurToken.s.sol
+++ script/DeployOurToken.s.sol
@@ -5,7 +5,7 @@ import {Script, console2} from "forge-std/Script.sol";
 import {OurToken} from "../src/OurToken.sol";
 
 contract DeployOurToken is Script {
-    uint256 public constant INITIAL_SUPPLY = 1_000_000 ether; // 1 million tokens with 18 decimal places
+    uint256 public constant INITIAL_SUPPLY = 100 ether; // 1 million tokens with 18 decimal places
 
     function run() external returns (OurToken) {
         vm.startBroadcast();
