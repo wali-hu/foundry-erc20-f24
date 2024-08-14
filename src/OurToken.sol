cba7239a068e6e87f7192b7a50912fcf17513a9a
Muhammad Abdullah <abdullah.se4th@gmail.com>
2024-08-14 17:27:39 +0500

    Added mint & burn function in OurToken for testing purposes

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -7,4 +7,14 @@ contract OurToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
         _mint(msg.sender, initialSupply);
     }
+
+    // Public mint function for testing purposes
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    // Public burn function for testing purposes
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
 }
